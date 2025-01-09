2adb756ce2f81b0463aa42d14386fd5bd955a157
Estampy <estanislaotorres@gmail.com>
2025-01-09 17:48:54 -0300

    agregado de repositorio flyway y configuracion.

--- src/main/java/med/voll/api/Controller/MedicoController.java
+++ src/main/java/med/voll/api/Controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.Controller;
 
 import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
-        System.out.println(datosRegistroMedico);
+        medicoRepository.save(new Medico(datosRegistroMedico));
     }
 }
