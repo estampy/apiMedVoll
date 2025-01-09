a338a716c8ab8454cc21514383c02faa8ae40db4
Estampy <estanislaotorres@gmail.com>
2025-01-09 16:49:34 -0300

    Agregando entidades medico y direccion, DTOs, Controller y application.properties conexion con BD

---
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -0,0 +1,9 @@
+package med.voll.api.direccion;
+
+public record DatosDireccion(
+        String calle,
+        String distrito,
+        String ciudad,
+        int numero,
+        String complemento) {
+}
